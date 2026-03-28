fb2ced0cddf05c5aa1046e720b0d9924bf1f1ef4
Ulzii0517 <ulziibadrakh.bb@gmail.com>
2026-03-28 21:49:27 +0800

    added suspence in bookspage

--- frontend/src/app/(user)/books/page.jsx
+++ frontend/src/app/(user)/books/page.jsx
@@ -1,10 +1,16 @@
+import { Suspense } from "react";
 import BookList from "@/components/user/books/BookList";
 
 const BooksPage = () => {
   return (
     <div className="flex flex-col justify-center items-center px-4 md:px-2 lg:px-11">
-      <BookList />
+      <Suspense
+        fallback={<div className="pt-40 text-white">Уншиж байна...</div>}
+      >
+        <BookList />
+      </Suspense>
     </div>
   );
 };
+
 export default BooksPage;
