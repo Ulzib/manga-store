1f963849dea3c3bc3d08302df437c8dfb1cc24dc
Ulzii0517 <ulziibadrakh.bb@gmail.com>
2026-03-29 16:45:33 +0800

    reveal whislist profile button on navbar

--- frontend/src/components/user/navigation/UserNavBar.jsx
+++ frontend/src/components/user/navigation/UserNavBar.jsx
@@ -122,7 +122,7 @@ export default function UserNavBar() {
             ) : (
               <Link
                 href="/login"
-                className="hidden md:block text-sm font-semibold text-white px-4 py-2 rounded-lg hover:text-blue-900 transition"
+                className=" text-sm font-semibold text-white px-4 py-2 rounded-lg hover:text-blue-900 transition"
               >
                 Нэвтрэх
               </Link>
