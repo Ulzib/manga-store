544183a0ccf78fae7fa1a242646a17a5c17a7611
Ulzii0517 <ulziibadrakh.bb@gmail.com>
2026-03-29 15:01:44 +0800

    fix: add dynamic/suspense to admin books 2

--- frontend/src/app/admin/books/page.jsx
+++ frontend/src/app/admin/books/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import React from "react";
 import Books from "../../../components/admin/books-com/Books";
 
--- frontend/src/app/admin/page.jsx
+++ frontend/src/app/admin/page.jsx
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import Dashboard from "@/components/admin/dashboard/Dashboard";
 
 const AdminHomePage = () => {
