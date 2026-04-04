d2bf8573968c42b0f90a646921399fab84934c00
Ulzii0517 <ulziibadrakh.bb@gmail.com>
2026-04-05 01:34:49 +0800

    admin bg color change

--- frontend/src/app/admin/layout.js
+++ frontend/src/app/admin/layout.js
@@ -2,7 +2,7 @@ import AdminNavBar from "../../components/admin/navigation/AdminNavBar";
 
 export default function AdminLayout({ children }) {
   return (
-    <div>
+    <div className="min-h-screen bg-white text-gray-900">
       <AdminNavBar />
       <main className=" mx-auto ">{children}</main>
     </div>
